5ef00a18bc56b0d87fb3709978d28775aa01d3a6
Jakub Sygnowski <sygnowski@gmail.com>
2016-02-14 15:14:34 +0100

    variational version

--- autoencoder.py
+++ autoencoder.py
@@ -1,4 +1,12 @@
 import matplotlib.pyplot as plt
+from fuel.datasets import MNIST
+from fuel.streams import DataStream
+from fuel.schemes import ShuffledScheme
+from fuel.transformers import Flatten
+from theano import tensor
+from blocks.bricks import Linear, Tanh
+from blocks.initialization import IsotropicGaussian, Constant
+from blocks.filter import VariableFilter
 
 def plot_images(orig, compressed, first):
   plt.subplot(121)
@@ -10,67 +18,61 @@ def plot_images(orig, compressed, first):
   else:
     plt.draw()
 
-# regular network
-from theano import tensor
-from blocks.bricks import Linear, Tanh
-from blocks.initialization import IsotropicGaussian, Constant
-x = tensor.matrix("features")
-input_to_latent = Linear(name="input_to_latent", input_dim=784, output_dim=30)
-input_to_latent.weights_init = IsotropicGaussian(0.01)
-input_to_latent.biases_init = Constant(0)
-input_to_latent.initialize()
-h = Tanh(name="transformation1").apply(input_to_latent.apply(x))
+def get_typical_layer(input_layer, input_dim, output_dim):
+  layer = Linear(input_dim=input_dim, output_dim=output_dim)
+  layer.weights_init = IsotropicGaussian(0.01)
+  layer.biases_init = Constant(0)
+  layer.initialize()
+  return Tanh().apply(layer.apply(input_layer))
 
-latent_to_output = Linear(name="latent_to_output", input_dim=30, output_dim=784)
-latent_to_output.weights_init = IsotropicGaussian(0.01)
-latent_to_output.biases_init = Constant(0)
-latent_to_output.initialize()
-output = Tanh(name="output").apply(latent_to_output.apply(h))
-
-from blocks.bricks.cost import SquaredError, AbsoluteError
-from blocks.graph import ComputationGraph
-from blocks.algorithms import GradientDescent, Scale
-from blocks.filter import VariableFilter
-from blocks.roles import WEIGHT
-
-cost = AbsoluteError().apply(x, output)
-cg = ComputationGraph(cost)
-gd = GradientDescent(cost=cost, parameters=cg.parameters, step_rule=Scale(learning_rate=0.01))
-
-# data
-from fuel.datasets import MNIST
-from fuel.streams import DataStream
-from fuel.schemes import ShuffledScheme
-from fuel.transformers import Flatten
-mnist = MNIST(("train", ), sources=["features"])
-ds = Flatten(DataStream.default_stream(mnist, iteration_scheme=\
-    ShuffledScheme(examples=mnist.num_examples, batch_size=10)))
-
+def get_data_stream(train=True):
+  mnist_id = "train" if train else "test"
+  mnist = MNIST((mnist_id, ), sources=["features"])
+  return Flatten(DataStream.default_stream(mnist, iteration_scheme=\
+      ShuffledScheme(examples=mnist.num_examples, batch_size=100)))
 # to get an example
 # example = ds.get_epoch_iterator().next()[0][0] # [sources][no-in-batch]
 
-from blocks.main_loop import MainLoop
-from blocks.extensions import FinishAfter, Printing, ProgressBar
-from blocks.extensions.monitoring import DataStreamMonitoring, TrainingDataMonitoring
-monitor = TrainingDataMonitoring([cost])
-main_loop = MainLoop(data_stream=ds, algorithm=gd, extensions=[monitor, FinishAfter(after_n_epochs=3),  ProgressBar(), Printing()])
-
-main_loop.run()
-
-mnist_test = MNIST(("test", ), sources=["features"])
-test_ds = Flatten(DataStream.default_stream(mnist, iteration_scheme=\
-    ShuffledScheme(examples=mnist.num_examples, batch_size=100)))
-
-def showcase():
+def showcase(cg, output_name="tanh_apply_output"):
   import numpy
   import time
   first = True
+  test_ds = get_data_stream(False)
   for image in next(test_ds.get_epoch_iterator())[0]:
-    cg2 = cg.replace({x: numpy.asmatrix(image)})
-    out, = VariableFilter(theano_name_regex="output_apply_output") (cg2.variables)
+    cg2 = cg.replace({cg.inputs[0]: numpy.asmatrix(image)})
+    out = (VariableFilter(theano_name_regex=output_name) (cg2.variables))[-1]
     plot_images(image, out.eval(), first)
     first = False
     time.sleep(1)
-#    plt.close()
+  plt.close()
 
-showcase()
+def main():
+  x = tensor.matrix("features")
+  input_to_hidden1 = get_typical_layer(x, 784, 500)
+  #hidden1_to_hidden2 = get_typical_layer(input_to_hidden1, 500, 300)
+  hidden1_to_latent = get_typical_layer(input_to_hidden1, 500, 20)
+
+  latent_to_hidden2 = get_typical_layer(hidden1_to_latent, 20, 500)
+  #hidden3_to_hidden4 = get_typical_layer(latent_to_hidden3, 300, 500)
+  hidden2_to_output = get_typical_layer(latent_to_hidden2, 500, 784)
+
+  from blocks.bricks.cost import SquaredError, AbsoluteError
+  from blocks.graph import ComputationGraph
+  from blocks.algorithms import Adam, GradientDescent, Scale
+  from blocks.roles import WEIGHT
+
+  cost = AbsoluteError(name="error").apply(x, hidden2_to_output)
+  cg = ComputationGraph(cost)
+  gd = GradientDescent(cost=cost, parameters=cg.parameters, step_rule=Adam())
+
+  from blocks.main_loop import MainLoop
+  from blocks.extensions import FinishAfter, Printing, ProgressBar
+  from blocks.extensions.monitoring import DataStreamMonitoring, TrainingDataMonitoring
+  monitor = TrainingDataMonitoring([cost], after_epoch=True)
+  main_loop = MainLoop(data_stream=get_data_stream(), algorithm=gd, extensions=[monitor, FinishAfter(after_n_epochs=4),  ProgressBar(), Printing()])
+
+  main_loop.run()
+  showcase(cg)
+
+if __name__ == "__main__":
+  main()
---
+++ variational.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+import time
+from autoencoder import get_data_stream, get_typical_layer, plot_images
+from theano import tensor
+from blocks.bricks import Linear, Tanh, Logistic
+from blocks.initialization import IsotropicGaussian, Constant
+from theano.sandbox.rng_mrg import MRG_RandomStreams
+
+L = 1
+BATCH_SIZE = 100
+J = 20 # latent dimension
+
+def plot_batch(orig, compressed):
+  for i in range(len(orig)):
+    plt.subplot(121)
+    plt.imshow(1 - orig[i].reshape(28, 28), cmap=plt.cm.gray)
+    plt.subplot(122)
+    plt.imshow(1 - compressed[i].reshape(28, 28), cmap=plt.cm.gray)
+    if i == 0:
+      plt.show(block=False)
+    else:
+      plt.draw()
+    time.sleep(1)
+  plt.close()
+
+def showcase(cg, output_name="last_apply_output"):
+  import numpy
+  first = True
+  test_ds = get_data_stream(False)
+  for image in next(test_ds.get_epoch_iterator()):
+    cg2 = cg.replace({cg.inputs[0]: numpy.asmatrix(image)})
+    out = (VariableFilter(theano_name_regex=output_name) (cg2.variables))[-1]
+    plot_batch(image, out.eval())
+  plt.close()
+
+def encoder_network(latent_dim=J):
+  x = tensor.matrix("features")
+  hidden1 = get_typical_layer(x, 784, 500)
+  log_sigma_sq = get_typical_layer(hidden1, 500, latent_dim)
+  mu = get_typical_layer(hidden1, 500, latent_dim)
+  return (log_sigma_sq, mu, x)
+
+def decoder_network(latent_sample, latent_dim=J):
+  # bernoulli case
+  hidden2 = get_typical_layer(latent_sample, latent_dim, 500)
+  hidden2_to_output = Linear(name="last", input_dim=500, output_dim=784)
+  hidden2_to_output.weights_init = IsotropicGaussian(0.01)
+  hidden2_to_output.biases_init = Constant(0)
+  hidden2_to_output.initialize()
+  return Logistic().apply(hidden2_to_output.apply(hidden2))
+
+rng = MRG_RandomStreams()
+
+def get_cost(latent_dim=J):
+  log_sigma_sq, mu, x = encoder_network(latent_dim)
+  sigma_sq = tensor.exp(log_sigma_sq)
+  eps = rng.normal((BATCH_SIZE, J))
+  z = tensor.sqrt(sigma_sq) + mu * eps
+  y = decoder_network(z) # TODO: L > 1
+  log_p_x_y = tensor.sum(tensor.log(y) * x + (1 - x) * tensor.log(1 - y))
+  KL = -0.5 * tensor.sum(1 + log_sigma_sq - mu ** 2 - sigma_sq)
+  cost = -tensor.mean(-KL + log_p_x_y) # over batch
+  cost.name = "cost"
+  return cost
+
+from blocks.graph import ComputationGraph
+from blocks.algorithms import GradientDescent, Scale, Adam
+from blocks.filter import VariableFilter
+from blocks.main_loop import MainLoop
+from blocks.extensions import FinishAfter, Printing, ProgressBar
+from blocks.extensions.monitoring import TrainingDataMonitoring
+
+cost = get_cost()
+cg = ComputationGraph(cost)
+gd = GradientDescent(cost=cost, parameters=cg.parameters,
+    step_rule=Adam())
+monitor = TrainingDataMonitoring([cost], after_epoch=True)
+main_loop = MainLoop(data_stream = get_data_stream(), algorithm=gd, extensions=[
+  monitor, FinishAfter(after_n_epochs=3), ProgressBar(), Printing()])
+
+main_loop.run()
+
+showcase(cg, "last_apply_output")
